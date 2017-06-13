6090971bd3a68fbc62a7ee4edd9c6ba9f30ff9af
Jacob <jacob@jacobgc.me>
2017-06-13 02:19:26 +0100

    Updated to support colour and added a load of images

--- routes/index.js
+++ routes/index.js
@@ -11,12 +11,45 @@ router.get('/', function (req, res, next) {
   res.render('index', {});
 });
 
-router.get('/*/*', function (req, res, next) {
-  var regex = /\/([0-9]*)\/([0-9]*)/g // Regex to look for /XXX/XXX (Where X is replaced with numbers)
+router.get('/c/*/*', function (req, res, next) {
+  genImageC(req, res, next);
+});
+
+router.get('/bw/*/*', function (req, res, next) {
+  genImageBW(req, res, next);
+});
+
+function genImageBW(req, res, next) {
+  var regex = /\/([0-9]*)\/([0-9]*)/g // Regex to look for /XXXX/XXXX (Where X is replaced with numbers)
   var imgW = parseInt(getMatches(req.url, regex, 1)[0]) || 0
   var imgH = parseInt(getMatches(req.url, regex, 2)[0]) || 0
-  if (!imgW > 0 || !imgH > 0 || !typeof imgW === 'number' || !typeof imgH === 'number'){
-    next("Error: Height/Width must be greater than 0 and not a string"); 
+  if (!imgW > 0 || !imgH > 0 || !typeof imgW === 'number' || !typeof imgH === 'number') {
+    next("Error: Height/Width must be greater than 0 and not a string");
+  }
+  if (imgW > 2000 | imgH > 2000)
+    next("Error: Height/Width must be less than or equal to 2000px");
+  var images = fs.readdirSync('public/img/raw/');
+  console.log(images.randomElement());
+  sharp('public/img/raw/' + images.randomElement())
+    .resize(parseInt(imgW), parseInt(imgH), {
+      kernel: sharp.kernel.nearest,
+    })
+    .greyscale()
+    .toBuffer()
+    .then(data => {
+      res.contentType('jpeg');
+      res.end(data);
+    }).catch(err => {
+      next(err);
+    });
+}
+
+function genImageC(req, res, next) {
+  var regex = /\/([0-9]*)\/([0-9]*)/g // Regex to look for /XXXX/XXXX (Where X is replaced with numbers)
+  var imgW = parseInt(getMatches(req.url, regex, 1)[0]) || 0
+  var imgH = parseInt(getMatches(req.url, regex, 2)[0]) || 0
+  if (!imgW > 0 || !imgH > 0 || !typeof imgW === 'number' || !typeof imgH === 'number') {
+    next("Error: Height/Width must be greater than 0 and not a string");
   }
   if (imgW > 2000 | imgH > 2000)
     next("Error: Height/Width must be less than or equal to 2000px");
@@ -33,7 +66,7 @@ router.get('/*/*', function (req, res, next) {
     }).catch(err => {
       next(err);
     });
-});
+}
 
 function getMatches(string, regex, index) {
   index || (index = 1); // default to the first capturing group
